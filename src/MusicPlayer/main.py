e82dd87c8ec29809018a08a53a358d667b661a21
WRmooney <mooneywilliam777@gmail.com>
2026-02-20 14:43:30 -0600

    Set up playback UI
    Added buttons, slider bar, track info, cover art, and back button

--- src/MusicPlayer/main.py
+++ src/MusicPlayer/main.py
@@ -4,11 +4,17 @@ kivy.require('2.3.1') # replace with your current kivy version !
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 
-
-class MusicMenu(Widget):
+class MusicMenu(BoxLayout):
     pass
-
 
 class MusicPlayerApp(App):
     def build(self):
